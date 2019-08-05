6a32ad691a7be9f32a2cce09dd455a2fa418e3e4
chasefulks <chase.fulks@gmail.com>
2019-08-05 17:30:43 -0500

    basichash

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -15,7 +15,9 @@ class Pair:
 # '''
 class BasicHashTable:
     def __init__(self, capacity):
-        pass
+       self.capacity = capacity
+       self.count = 0
+       self.storage = [None] * capacity
 
 
 # '''
@@ -23,7 +25,10 @@ class BasicHashTable:
 # Research and implement the djb2 hash function
 # '''
 def hash(string, max):
-    pass
+    hash = 5381
+    for i in string:
+        hash = ((hash << 5) + hash) + ord(i)
+    return hash % max
 
 
 # '''
@@ -32,7 +37,10 @@ def hash(string, max):
 # If you are overwriting a value with a different key, print a warning.
 # '''
 def hash_table_insert(hash_table, key, value):
-    pass
+    index = hash(key, hash_table.capacity)
+
+    hash_table.storage[index] = value
+
 
 
 # '''
@@ -41,7 +49,9 @@ def hash_table_insert(hash_table, key, value):
 # If you try to remove a value that isn't there, print a warning.
 # '''
 def hash_table_remove(hash_table, key):
-    pass
+    index = hash(key, hash_table.capacity)
+
+    hash_table.storage[index] = None
 
 
 # '''
@@ -50,7 +60,9 @@ def hash_table_remove(hash_table, key):
 # Should return None if the key is not found.
 # '''
 def hash_table_retrieve(hash_table, key):
-    pass
+    index = hash(key, hash_table.capacity)
+
+    return hash_table.storage[index]
 
 
 def Testing():
